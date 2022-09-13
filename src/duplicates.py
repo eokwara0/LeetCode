ed8d7f587eaa408a91c8409bad3f848f1985647a
emmanuel <eokwara021@student.wethinkcode.co.za>
2022-09-13 10:57:14 +0200

    chore:duplicates

--- src/duplicates.py
+++ src/duplicates.py
@@ -1,26 +1,21 @@
-
+import collections
 from timeit import timeit
 
 class Solution:
     def containsDuplicate( self , nums: list[int]) -> bool :
         
-        
+        counts = collections.Counter( nums )
+        for value in counts.values():
+            if value > 1:
+                return True 
                 
         return False 
     
         
-    def list_operation( self , nums: list[int] , limit : int, value : int  ) -> bool  :
-        print( nums , value )
-        if value in nums or len(nums) <= limit  :
-            return True
-        else :
-            return self.list_operation( nums[ : len(nums)//2] , limit , value  )
     
     
     
     
     
 if __name__ == "__main__":
-    solution = Solution()
-    data = [1,2,3,4]
-    print(timeit( lambda : solution.containsDuplicate( data ) , number= 1 ))
+    print(timeit( lambda : Solution().containsDuplicate( [1,2,3,4 ]) , number=1))
