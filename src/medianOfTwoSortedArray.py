190bfa5490becd64f1c0e29914db63ab2ea94911
emmanuel <emmanuelokwara001@gmail.com>
2023-01-16 20:24:30 +0200

    add:MedianofTwoSortedArray.py

--- src/medianOfTwoSortedArray.py
+++ src/medianOfTwoSortedArray.py
@@ -2,16 +2,22 @@
 
 class Solution:
     def findMedianSortedArrays(self, nums1 : list , nums2 : list ) -> float:
-        len_a1 = len(nums1);
-        len_a2 = len(nums2);
+        len_a1 = len(nums1)
+        len_a2 = len(nums2)
+        nums = sorted(nums1 + nums2)
+        sums = len_a1 + len_a2
+        midIndex = sums // 2
 
-        if len_a1 %2 != 0 and len_a2 %2 == 0:
-            return float( nums1[-1] )
-        elif len_a1 %2 == 0 and len_a2 %2 !=0 and ( len_a1 != 0 or len_a2 != 0 ) :
-            return float( nums2[-1] )
-        elif len_a1 %2 == 0 and len_a2 %2 == 0:
-            return float((nums1[-1] + nums2[-1])/2 )
-        elif len_a1 %2 != 0 and len_a2 %2 != 0 :
-            newList = nums1 + nums2
-            lenList = len( newList )
-            if lenList == 0:
+        if sums % 2 != 0:
+            return float(nums[midIndex])
+        elif ( sums % 2 ) == 0 :
+            return float((nums[midIndex] + nums[midIndex -1])) / 2
+
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    solution = sol.findMedianSortedArrays([1,2,3,3,5,34,3,34,32,3] , [4 , 5 , 6 ])
+
+    print( solution )
+        
