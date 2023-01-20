121a5e0710082845b2bb3920998d02ce17655db1
emmanuel <emmanuelokwara001@gmail.com>
2023-01-20 13:04:57 +0200

    IntToRoman - converting integers to roman numerals

--- src/int_to_roman.py
+++ src/int_to_roman.py
@@ -6,20 +6,20 @@ from timeit import timeit
 class Solution:
     
     __symbol = { "I" : 1 , "IV": 4 , "V": 5 , "IX": 9 , "X": 10 , "XL": 40 , "L": 50 , "XC" : 90 , "C" : 100  , "CD" : 400 , "D" : 500 , "CM" : 900 , "M" : 1000 }
-    __data = reversed(__symbol.keys())
+    __data = list(__symbol.keys())[::-1]
     
     
     def int_to_roman( self , num : int ) -> str:  # type: ignore
-        
-        if num <= 0 : 
+        key_ = ""
+        if num <= 0:
             return ""
-        else:
+        else :
             for key in self.__data:
                 if num % self.__symbol[key] < num  :
                     num -= self.__symbol[key]
-                    return key  + self.int_to_roman( num )
-                
-        
-        
+                    key_ = key
+                    return key_  + self.int_to_roman( num )
+
 if __name__ == '__main__':
-    print(timeit( lambda : Solution().int_to_roman( 1994 ) , number=1))
+    for i in range(1,20,2):
+        print(Solution().int_to_roman(i))
