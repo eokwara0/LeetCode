53232c4b1a143df7c87bc7a1eaa74e80bb5472e6
emmanuel <emmanuelokwara001@gmail.com>
2023-09-09 21:22:08 +0200

    test: :construction: sip tests
    Added two tests for sip problem leetcode

--- practice.py
+++ practice.py
@@ -1,1 +1,25 @@
-print(753865680 + 798580876)
+class Solution:
+    def searchInsert( self, nums :list, target: int):
+
+        counter = 0
+        list_length = len(nums)-1
+        while counter <= list_length:
+            if nums[counter] == target:
+                return counter
+            elif nums[list_length - counter ] == target:
+                return list_length - counter
+            elif target < nums[counter]:
+                return counter
+            elif target - nums[counter] == 1:
+                    return counter + 1
+            elif target > nums[list_length - counter]:
+                    return (list_length - counter)+1
+            counter = counter + 1
+        return counter
+        
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    index = solution.searchInsert([1], 0)
+    print(index)
---
+++ src/search_insert_position/search_insert_position.py
@@ -0,0 +1,30 @@
+'''
+ Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+'''
+
+class Solution:
+    
+    def searchInsert( self, nums :list, target: int):
+
+        counter = 0
+        list_length = len(nums)-1
+        while counter <= list_length:
+            if nums[counter] == target:
+                return counter
+            elif nums[list_length - counter ] == target:
+                return list_length - counter
+            elif target < nums[counter]:
+                return counter
+            elif target - nums[counter] == 1:
+                    return counter + 1
+            elif target > nums[list_length - counter]:
+                    return (list_length - counter)+1
+            counter = counter + 1
+        return counter
+        
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    index = solution.searchInsert([1], 0)
+    print(index)
